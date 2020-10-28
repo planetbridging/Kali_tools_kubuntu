f8297392410c5498915c2f33de187bc8a58106e9
planetbridging <47130739+planetbridging@users.noreply.github.com>
2020-10-28 18:56:04 +1000

    Update kali_install.py

--- kali_install.py
+++ kali_install.py
@@ -1,14 +1,20 @@
 import os
 import sys, traceback
+import subprocess
 
 
 
 #os.system('ls -l')
+failed = []
 
-
+def Check_Install(app):
+	p = subprocess.Popen("dpkg -l |grep " + app, stdout=subprocess.PIPE, shell=True)
+	(output, err) = p.communicate()
+	p_status = p.wait()
+	return output
 
 def Install_Packages(txt):
-
+	
 	file1 = open(txt, 'r') 
 	Lines = file1.readlines() 
 	  
@@ -16,10 +22,21 @@ def Install_Packages(txt):
 	linecount = len(Lines)
 	# Strips the newline character 
 	for line in Lines: 
-		cmd = 'apt-get install ' + line.strip() + ' -y'
-		os.system(cmd)
+		app = line.strip()
+		cmd = 'apt-get install ' + app + ' -y'
+		if len(Check_Install(app)) == 0:
+			os.system(cmd)
+			check = Check_Install(app)
+			if len(check) == 0:
+				os.system(cmd.lower())
+				checkagain = Check_Install(app.lower())
+				if len(checkagain) == 0:
+					failed.append(app)
+
+
+		#print(osout.len)
 		count+=1
-		print( str(count) + "/" + str(linecount))
+		#print( str(count) + "/" + str(linecount))
 	print("----------------------------------")
 	print(txt + " done")
 	print("----------------------------------")
@@ -37,3 +54,10 @@ Install_Packages('hardware_hacking.txt')
 Install_Packages('reporting_tools.txt')
 Install_Packages('maintaining_access.txt')
 Install_Packages('reverse_engineering.txt')
+
+print("----------------------------------")
+print("Finished!!!!")
+print("----------------------------------")
+
+for f in failed:
+	print("Failed to install: " + f)
