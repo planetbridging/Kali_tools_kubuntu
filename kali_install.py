9725a28285429ddd51ef52ce192b012516557cfd
planetbridging <47130739+planetbridging@users.noreply.github.com>
2020-10-29 15:31:37 +1000

    Update kali_install.py

--- kali_install.py
+++ kali_install.py
@@ -1,63 +1,100 @@
 import os
 import sys, traceback
 import subprocess
+import os.path
 
+
+
+class objInstaller:
+	def __init__(self):
+		print("Welcome to security tools installer for ubuntu")
+		self.failed = []
+		self.full_total = 0
+		self.AllPackages = [
+		'information_gathering', 'vulnerability_analysis',
+		'wireless_attacks', 'web_applications',
+		'exploitation_tools', 'stress_testing',
+		'forensics_tools', 'sniffing_and_spoofing',
+		'password_attacks', 'hardware_hacking',
+		'reporting_tools', 'maintaining_access',
+		'reverse_engineering']
+		self.full_path = os.getcwd()
+		self.Cmd_Loop()
+
+	def Cmd_Loop(self):
+		while True:
+			i = input(">")
+			if i == "quit":
+				print("bubye")
+				break
+			elif i == "ca":
+				self.Check_All()
+			elif i == "ia":
+				self.Install_all()
+
+	def Check_All(self):
+		full_count = 0
+		total_count = 0
+		for p in self.AllPackages:
+			#p = self.AllPackages[0]
+			file_path = self.full_path + "/" + p + ".txt"
+			#print(os.path.isfile(file_path))
+			file1 = open(file_path, 'r') 
+			Lines = file1.readlines() 
+			count = 0
+			total = len(Lines)
+			total_count += len(Lines)
+			for line in Lines:
+				if " " in line:
+					print("unsure: " + line)
+				else:
+					a = self.Check_Install(line)
+					if len(a) > 0:
+						count += 1
+						full_count += 1
+			print("----------------------------------")
+			print(p + " " + str(count) + "/" + str(total))
+			print("----------------------------------")
+		print("----------------------------------")
+		print("complete " + str(count) + "/" + str(total_count))
+		print("----------------------------------")
+		
+
+	def Install_all(self):
+		for p in self.AllPackages:
+			#p = self.AllPackages[0]
+			file_path = self.full_path + "/" + p + ".txt"
+			self.Install_Packages(file_path)
+		self.Check_All()
 
 
 #os.system('ls -l')
-failed = []
 
-def Check_Install(app):
-	p = subprocess.Popen("dpkg -l |grep " + app, stdout=subprocess.PIPE, shell=True)
-	(output, err) = p.communicate()
-	p_status = p.wait()
-	return output
 
-def Install_Packages(txt):
+	def Check_Install(self,app):
+		p = subprocess.Popen("dpkg -l |grep " + app, stdout=subprocess.PIPE, shell=True)
+		(output, err) = p.communicate()
+		p_status = p.wait()
+		return output
+
+	def Install_Packages(self,txt):
+		
+		file1 = open(txt, 'r') 
+		Lines = file1.readlines() 
+		
+		count = 0
+		linecount = len(Lines)
+		# Strips the newline character 
+		for line in Lines: 
+			app = line.strip()
+			if " " not in app:
+				cmd = 'apt-get install ' + app + ' -y'
+				if len(self.Check_Install(app)) == 0:
+					os.system(cmd)
+					check = self.Check_Install(app)
+					if len(check) == 0:
+						os.system(cmd.lower())
+
 	
-	file1 = open(txt, 'r') 
-	Lines = file1.readlines() 
-	  
-	count = 0
-	linecount = len(Lines)
-	# Strips the newline character 
-	for line in Lines: 
-		app = line.strip()
-		cmd = 'apt-get install ' + app + ' -y'
-		if len(Check_Install(app)) == 0:
-			os.system(cmd)
-			check = Check_Install(app)
-			if len(check) == 0:
-				os.system(cmd.lower())
-				checkagain = Check_Install(app.lower())
-				if len(checkagain) == 0:
-					failed.append(app)
-
-
-		#print(osout.len)
-		count+=1
-		#print( str(count) + "/" + str(linecount))
-	print("----------------------------------")
-	print(txt + " done")
-	print("----------------------------------")
-
-Install_Packages('information_gathering.txt')
-Install_Packages('vulnerability_analysis.txt')
-Install_Packages('wireless_attacks.txt')
-Install_Packages('web_applications.txt')
-Install_Packages('exploitation_tools.txt')
-Install_Packages('stress_testing.txt')
-Install_Packages('forensics_tools.txt')
-Install_Packages('sniffing_and_spoofing.txt')
-Install_Packages('password_attacks.txt')
-Install_Packages('hardware_hacking.txt')
-Install_Packages('reporting_tools.txt')
-Install_Packages('maintaining_access.txt')
-Install_Packages('reverse_engineering.txt')
-
-print("----------------------------------")
-print("Finished!!!!")
-print("----------------------------------")
-
-for f in failed:
-	print("Failed to install: " + f)
+if __name__ == "__main__":
+	newobj = objInstaller()
